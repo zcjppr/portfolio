3a8b9e324aef78ba3e2e85e15289319d87e2c72e
Pengyu <randychen401@gmail.com>
2023-05-18 13:23:57 -0700

    1

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
   reactStrictMode: false,
   images: {
     domains: ['avatars.githubusercontent.com'],
