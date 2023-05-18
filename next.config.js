8fa313bdf009422b400967f89e79e2c6a24e5bd8
zcjppr <134008228+zcjppr@users.noreply.github.com>
2023-05-18 13:19:04 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
   reactStrictMode: false,
   images: {
     domains: ['avatars.githubusercontent.com'],
